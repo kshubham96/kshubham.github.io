c78cba105e95cae6aa9feb1096ec77dec0464cf3
Shubham Khaladkar <51665593+kshubham96@users.noreply.github.com>
2023-07-31 09:25:44 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@
           var custom_greet = document.querySelector('#intro-greeting');
           custom_greet.style.display = 'block';
           var fullName = document.querySelector('#full-name');
-          fullName.innerHTML = 'Uday Kanth Reddy';
+          fullName.innerHTML = 'Shubham Khaladkar';
           $('#header').removeClass('header-top');
           $('section').removeClass('section-show');
           return;
